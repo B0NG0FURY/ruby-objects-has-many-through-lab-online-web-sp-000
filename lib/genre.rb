95288dd790bede3521562b882377fbef59e29acb
John Troutman <jtroutmanman@gmail.com>
2020-10-05 04:27:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,17 @@
 class Genre
+  attr_accessor :genre
+  @@all = []
   
+  def initialize(genre)
+    @genre = genre
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
 end
